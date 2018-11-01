e866076d9f69672cb53936f18ce259c91faa3128
DESKTOP-DR4T9QF\kmang <katelyn.mangold@my.simpson.edu>
2018-10-31 20:15:10 -0500

    Changed the nav bar html to php

--- public_html/navigation.php
+++ public_html/navigation.php
@@ -1,5 +1,5 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="index.html">Iowa Cornhuskers</a>
+      <a class="navbar-brand" href="index.php">Iowa Cornhuskers</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -7,28 +7,28 @@
 
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="roster.html">Roster</a>
+            <a class="nav-link" href="roster.php">Roster</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="schedule.html">Schedule</a>
+            <a class="nav-link" href="schedule.php">Schedule</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="coaches.html">Coaches</a>
+            <a class="nav-link" href="coaches.php">Coaches</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="news.html">News</a>
+            <a class="nav-link" href="news.php">News</a>
           </li>
 
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="sign_up.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="sign_up.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Sign Up
             </a>
              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="sign_up.html">Sign Up</a>
-              <a class="dropdown-item" href="login.html">Login</a>
+              <a class="dropdown-item" href="sign_up.php">Sign Up</a>
+              <a class="dropdown-item" href="login.php">Login</a>
             </div>
           </li>
         </ul>
